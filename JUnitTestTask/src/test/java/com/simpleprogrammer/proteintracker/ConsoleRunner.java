ff465946a2604f860ead9cfa06d0aa134bb143fb
RAMIL ARSAEV <gattoramm@gmail.com>
2022-08-15 09:22:03 +0300

    add ConsoleRunner

---
+++ JUnitTestTask/src/test/java/com/simpleprogrammer/proteintracker/ConsoleRunner.java
@@ -0,0 +1,13 @@
+package com.simpleprogrammer.proteintracker;
+
+import org.junit.internal.TextListener;
+import org.junit.runner.JUnitCore;
+
+public class ConsoleRunner {
+    public static void main(String[] args) {
+        JUnitCore jUnit = new JUnitCore();
+        jUnit.addListener(new TextListener(System.out));
+
+        jUnit.run(TrackingServiceTest.class);
+    }
+}
